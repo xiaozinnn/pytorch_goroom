199b4962714c8fc708cff029bf81721a36c918da
xiaozinnnn <1605979488@qq.com>
2024-11-26 16:19:44 +0800

    pytorch_romen

--- 2_tensorboard.py
+++ 2_tensorboard.py
@@ -1,4 +1,4 @@
-# tensorboard可视化的使用
+# tensorboard可视化的使用1
 from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 from PIL import Image
@@ -8,7 +8,6 @@ img_path = "dataset/train/ants_image/116570827_e9c126745d.jpg"
 img_PIL = Image.open(img_path)
 img_array = np.array(img_PIL)
 print(type(img_array))
-print(img_array.shape)
 
 writer.add_image("tran", img_array, 3, dataformats='HWC')
 for i in range(100):
